a8585fb91f3f0e8a99d5770cf419e2112a8f5585
ysk3da <ysk3da@icloud.com>
2023-12-22 17:16:56 +0900

    動画内容をコンプリート

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Vite react側から svelte in reactを実現しようとしたが、うまく行かずだったので、逆に vite svelteではじめてreactを入れてみる
 
 Wrapper関数を定義する方法は別の方法を模索すべき（React HooksがCallback関数の中で呼べないルールなので）
+
+- [参考動画](https://www.youtube.com/watch?v=FrusJNycQvk)
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
 import React, { useLayoutEffect, useRef } from 'react';
 import './App.css';
+import Counter from "./Counter.svelte";
 import Hello from "./Hello.svelte";
 import useStore from "./store";
 
 function App() {
   const { count, increment }= useStore();
 
-  const svelteRef = useRef<HTMLDivElement>(null);
+  function SvelteWrapper(Component){
+      const svelteRef = useRef<HTMLDivElement>(null);
+      return (props) => {
+        // このAppがマウントされた直後に呼び出される
+        useLayoutEffect(()=>{
+          // React.StrictModeでは 2回LayoutEffectが走る 最も簡単な方法はfistChildを確認して、あれば削除する方法
+          while (svelteRef.current?.firstChild) {
+            svelteRef.current?.firstChild?.remove();
+          }
+          if(svelteRef.current) {
+            new Component({
+              target: svelteRef.current,
+              props
+            })
+          }
+        }, [])
+        return <div ref={svelteRef}></div>
+      }
+  }
   
-  // このAppがマウントされた直後に呼び出される
-  useLayoutEffect(()=>{
-    // React.StrictModeでは 2回LayoutEffectが走る 最も簡単な方法はfistChildを確認して、あれば削除する方法
-    while (svelteRef.current?.firstChild) {
-      svelteRef.current?.firstChild?.remove();
-    }
-    if(svelteRef.current) {
-      new Hello({
-        target: svelteRef.current,
-        props: {
-          extraText: "here is React",
-          onClick: increment
-        }
-      })
-    }
-  }, [])
-  
+  const SvelteHello = SvelteWrapper(Hello)
+  const SvelteCounter = SvelteWrapper(Counter)
   
   return (
     <>
       <div>
-        <div ref={svelteRef}></div>
+        <SvelteHello
+          extraText="Here is React!!!"
+          onClick={increment}
+         />
+         <SvelteCounter/>
         <button className='btn btn-success' onClick={increment}>
           count is {count}
         </button>
